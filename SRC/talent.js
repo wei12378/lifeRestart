274162023f2a1255746fd37d2c3a60a7c288175e
wei12378 <90132628+wei12378@users.noreply.github.com>
2021-09-05 17:13:57 +0800

    Update and rename src/talent.js to SRC/talent.js

--- SRC/talent.js
+++ SRC/talent.js
@@ -55,14 +55,14 @@ class Talent {
             else talentList[grade].push({ grade, name, description, id });
         }
 
-        return new Array(10)
+        return new Array(30)
             .fill(1).map((v, i)=>{
                 if(!i && include) return include;
                 const gradeRandom = Math.random();
                 let grade;
-                if(gradeRandom>=0.111) grade = 0;
-                else if(gradeRandom>=0.011) grade = 1;
-                else if(gradeRandom>=0.001) grade = 2;
+                if(gradeRandom>=2.111) grade = 0;
+                else if(gradeRandom>=2.011) grade = 1;
+                else if(gradeRandom>=0.5) grade = 2;
                 else grade = 3;
 
                 while(talentList[grade].length == 0) grade--;
